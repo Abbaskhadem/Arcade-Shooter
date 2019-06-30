f70f251fbc835f00caf3f59a90e9e9ca0c7e6638
AbbasKhadem <abbas.khadem.alhossini0121@gmail.com>
2019-06-30 12:46:40 +0430

    Grid System

--- Arcade-Shooter/Assets/Scripts/GameManager.cs
+++ Arcade-Shooter/Assets/Scripts/GameManager.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject Temp;
+    float Width;
+    float Height;
+    [SerializeField]
+    Camera Cam;
     public static bool GameLost=false;
     // Start is called before the first frame update
     void Start()
     {
+        Height = 2f * Cam.orthographicSize;
+        Width = Height * Cam.aspect;
+        //Debug.Log(Width);
+        //Debug.Log(SetXGrid());
         
+        //Temp.GetComponent<Transform>().localScale = new Vector3(Width, Height, 0);
+        Instantiate(Temp, GG(0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -20,5 +32,22 @@ public class GameManager : MonoBehaviour
     {
         GameLost = true;
     //    Debug.Log("Game Has Ended!");
+    }
+    float SetXGrid()
+    {
+        return Width / 2f;
+    }
+    float SetYGrid()
+    {
+        return Height / 2f;
+    }
+    Vector2 GG(int a, int b)
+    {
+        float aa = a*SetXGrid() - Width/2;
+        float bb = (-1)*(b*SetYGrid() - Height/2);
+        //aa = aa + (SetXGrid() / 2f);
+        //bb = bb - (SetYGrid() / 2f);
+        Vector2 natije = new Vector2(aa, bb);
+        return natije;
     }
 }
