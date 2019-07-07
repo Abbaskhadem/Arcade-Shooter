3dc9f33971e8ab72d07659a0d804b78a71c95b82
3ppehr <sepehr.asghari1377@gmail.com>
2019-07-07 10:21:46 +0430

    BasicMovmentEnemy

---
+++ Arcade-Shooter/Assets/Scripts/BezierFallow.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierFallow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] routes;
+    private int RouteToGo;
+    private float tparam;
+    private Vector2 Enemyposition;
+    private float SpeedModifier;
+    private bool coroutineAllowed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RouteToGo = 0;
+        tparam = 0;
+        SpeedModifier = 0.5f;
+        coroutineAllowed = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coroutineAllowed)
+            StartCoroutine(GoByTheRoute(RouteToGo));
+        
+    }
+    private IEnumerator GoByTheRoute(int RouteNumber)
+    {
+        coroutineAllowed = false;
+        Vector2 p0 = routes[RouteNumber].GetChild(0).position;
+        Vector2 p1 = routes[RouteNumber].GetChild(1).position;
+        Vector2 p2 = routes[RouteNumber].GetChild(2).position;
+        Vector2 p3 = routes[RouteNumber].GetChild(3).position;
+        while (tparam < 1)
+        {
+            tparam += Time.deltaTime * SpeedModifier;
+            Enemyposition = Mathf.Pow(1 - tparam, 3) * p0 +
+                3 * Mathf.Pow(1 - tparam, 2) * tparam * p1 +
+                3 * (1 - tparam) * Mathf.Pow(tparam, 2) * p2 +
+                Mathf.Pow(tparam, 3) * p3;
+            transform.position = Enemyposition;
+            yield return new WaitForEndOfFrame();
+
+        }
+        tparam = 0f;
+        RouteToGo += 1;
+        if (RouteToGo > routes.Length - 1)
+            RouteToGo = 0;
+        coroutineAllowed = true;
+    }
+}
---
+++ Arcade-Shooter/Assets/Scripts/Rout.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rout : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] ControlPoints;
+    private Vector2 GizmosPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnDrawGizmos()
+    {
+        for(float t = 0; t <= 1; t += 0.05f)
+        {
+            GizmosPosition = Mathf.Pow(1 - t, 3) * ControlPoints[0].position
+                + 3 * Mathf.Pow(1 - t, 2) * t * ControlPoints[1].position
+                + 3 * (1 - t) * Mathf.Pow(t, 2) * ControlPoints[2].position +
+                Mathf.Pow(t, 3) * ControlPoints[3].position;
+            Gizmos.DrawSphere(GizmosPosition, 0.25f);
+
+        }
+        Gizmos.DrawLine(new Vector2(ControlPoints[0].position.x, ControlPoints[0].position.y), new Vector2(ControlPoints[1].position.x, ControlPoints[1].position.y));
+        Gizmos.DrawLine(new Vector2(ControlPoints[2].position.x, ControlPoints[2].position.y), new Vector2(ControlPoints[3].position.x, ControlPoints[3].position.y));
+    }
+}
