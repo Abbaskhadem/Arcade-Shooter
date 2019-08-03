269fb6c320d4c1049f5d7bde290172f9539db3b9
3ppehr <sepehr.asghari1377@gmail.com>
2019-08-03 18:57:38 +0430

    wave enemy

--- Arcade-Shooter/Assets/Scripts/Core_Scripts/Enemy_Controller.cs
+++ Arcade-Shooter/Assets/Scripts/Core_Scripts/Enemy_Controller.cs
@@ -11,14 +11,15 @@ public class Enemy_Controller : MonoBehaviour
     Transform WhereToSpawn;
     int j = 0;
     int k = 0;
+    int i = 0;
     int frame;
     [System.Serializable]
     public class Wave
     {
-        public GameObject Objects;
+        public GameObject[] Objects;
         public float TimeBetweenEnemies;
         public List<GameObject> EnemiesList;
-        public int MaxEnemies;
+        public int[] MaxEnemies;
     }
 
     public Wave[] _Waves;
@@ -57,16 +58,19 @@ public class Enemy_Controller : MonoBehaviour
             waveType++;
             for (j = 0; j < _Waves.Length; j++)
             {
-                
-                _Waves[j].EnemiesList = GameManager.ObjectPooler(_Waves[j].Objects, _Waves[j].MaxEnemies);
-
-                for (int i = 0; i < _Waves[j].MaxEnemies; i++)
+                for (int b = 0; b < _Waves[j].Objects.Length; b++)
                 {
-                    _Waves[i].Objects.transform.position = spawnPosition;
-                    _Waves[i].Objects.transform.rotation = spawnRotation;
-                    _Waves[j].EnemiesList[i].SetActive(true);
-                    yield return new WaitForSeconds(_Waves[j].TimeBetweenEnemies);
+                    _Waves[b].EnemiesList = GameManager.ObjectPooler(_Waves[j].Objects[b], _Waves[j].MaxEnemies[b]);
 
+                    for ( i = 0; i < _Waves[j].MaxEnemies.Length; i++)
+                    {
+                        // _Waves[i].Objects.transform.position = spawnPosition;
+                        // _Waves[i].Objects.transform.rotation = spawnRotation;
+                         _Waves[j].EnemiesList[i].SetActive(true);
+
+                        yield return new WaitForSeconds(_Waves[j].TimeBetweenEnemies);
+                    }
+                    yield return new WaitForSeconds(_Waves[j].TimeBetweenEnemies);
                 }
 
                 yield return new WaitUntil(() => frame >= 1);
@@ -75,7 +79,7 @@ public class Enemy_Controller : MonoBehaviour
         }
 
     }
-     bool CheckAlive()
+    bool CheckAlive()
     {
         if (GameObject.FindGameObjectWithTag("Enemy") == null)
             return false;
