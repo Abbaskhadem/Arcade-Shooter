848199056abf69700b7a99811d51b867e0eee429
3ppehr <36326467+3ppehr@users.noreply.github.com>
2019-08-11 11:06:21 +0430

    Clean

--- Arcade-Shooter/Assets/Scripts/Core_Commponent/Bullet.cs
+++ Arcade-Shooter/Assets/Scripts/Core_Commponent/Bullet.cs
@@ -10,12 +10,6 @@ public class Bullet : MonoBehaviour
     [SerializeField] private ParticleSystem BulletEffect;
 #endregion
 #region Bullet Methods
-
-void Start()
-{
-//    if(gameObject.tag=="Player Bullet")
-//    BulletEffect = Instantiate(BulletEffect);
-}
 void Update()
     {
         objectRigidbody = transform.GetComponent<Rigidbody2D>();
