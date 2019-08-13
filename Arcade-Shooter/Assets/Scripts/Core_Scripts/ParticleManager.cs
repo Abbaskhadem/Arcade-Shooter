45e36ec771d91695064c2a018ac50487fbcce737
Abbas Khadem <33686471+Abbaskhadem@users.noreply.github.com>
2019-08-13 10:46:23 +0430

    Effects Edited

--- Arcade-Shooter/Assets/Scripts/Core_Scripts/ParticleManager.cs
+++ Arcade-Shooter/Assets/Scripts/Core_Scripts/ParticleManager.cs
@@ -7,11 +7,15 @@ public class ParticleManager : MonoBehaviour
 {
     public static ParticleManager _Instance;
     public ParticleSystem PlayerParticles;
+    public ParticleSystem EnemyParticles;
     public ParticleSystem tempParticle;
+    public ParticleSystem tempParticle2;
     void Start()
     {
         tempParticle = Instantiate(PlayerParticles);
+        tempParticle2 = Instantiate(EnemyParticles);
         tempParticle.transform.position=new Vector3(10,10,0);
+        tempParticle2.transform.position=new Vector3(10,10,0);
     }
 
     void Awake()
