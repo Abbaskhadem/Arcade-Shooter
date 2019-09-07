99d32b0da1c0e2ef69a196f5e7716f65d7954cc3
3ppehr <36326467+3ppehr@users.noreply.github.com>
2019-09-07 09:34:28 +0430

    Random&Shop

--- Arcade-Shooter/Assets/Scripts/Core_Scripts/Random_Director.cs
+++ Arcade-Shooter/Assets/Scripts/Core_Scripts/Random_Director.cs
@@ -6,15 +6,18 @@ public class Random_Director : MonoBehaviour
 {
     #region Temp Variables
 
+    private float AttackTimer;
     public bool Pause = false;
     private int temp1;
     bool UpgrateWave = false;
     int LastRandom = 0;
     int FirstRandom = 0;
     int RandomWave = 0;
+    
     int i = 0;
-    private bool dd=false;
-    private int a;
+    private bool dd = false;
+    private int temp;
+    private List<GameObject> activEnemy;
     private bool SpawnAllowed = true;
     private int WaveNumber;
     bool firsttime = false;
@@ -35,7 +38,10 @@ public class Random_Director : MonoBehaviour
         public int[] Quantity;
         public float ActiveDly;
         public List<GameObject> EnemyList;
+        public bool spetiallRout;
+        public Transform[] Routes;
         public Transform[] FinalPositions;
+        
     }
 
     #endregion
@@ -53,6 +59,22 @@ public class Random_Director : MonoBehaviour
     {
         CheckAlive();
         if (UpgrateWave) GetRandomWaveIndex();
+        if (!SpawnAllowed)
+        {
+            AttackTimer -= Time.deltaTime;
+            if (AttackTimer <= 0)
+            {
+                AttackTimer = Random.Range(1.5f, 2f); 
+                temp= Random.Range(0, Waves[RandomWave].EnemyList.Count);
+                if (Waves[RandomWave].EnemyList[temp] != null)
+                {
+                    if(Waves[RandomWave].EnemyList[temp].activeInHierarchy)
+                        Waves[RandomWave].EnemyList[temp].GetComponent<Enemy_SpaceShip>().Shoot();
+                }
+            }
+
+      
+        }
     }
 
     #endregion
@@ -62,60 +84,100 @@ public class Random_Director : MonoBehaviour
     IEnumerator SpawnEnemyWaves(int a)
     {
         if (!Pause)
+        {
+            if (SpawnAllowed)
             {
-                if (SpawnAllowed)
+                if (Waves[a].EnemyList.Count == 0)
                 {
-                    if (Waves[a].EnemyList.Count == 0)
+                    for (int j = 0; j < Waves[a].EnemyTypes.Length; ++j)
                     {
-                        for (int j = 0; j < Waves[a].EnemyTypes.Length; ++j)
+                        for (int k = 0; k < Waves[a].Quantity[j]; k++)
                         {
-                            for (int k = 0; k < Waves[a].Quantity[j]; k++)
+                            if (!Pause)
                             {
-                                if (!Pause)
+                               
+                                RandomFinalPositions = Random.Range(0, Waves[a].FinalPositions.Length);
+                                GameObject temp = (GameObject) Instantiate(Waves[a].EnemyTypes[j]);
+                                temp.SetActive(false);
+                                Waves[a].EnemyList.Add(temp);
+                                temp.GetComponent<Enemy_SpaceShip>().MoveAllowed = true;
+                                temp.GetComponent<Enemy_SpaceShip>().FinalDestination =
+                                    Waves[a].FinalPositions[RandomFinalPositions].GetChild(temp1++);
+                                if (Waves[a].spetiallRout)
                                 {
-                                    RandomFinalPositions = Random.Range(0, Waves[a].FinalPositions.Length);
-                                    GameObject temp = (GameObject) Instantiate(Waves[a].EnemyTypes[j]);
-                                    temp.SetActive(false);
-                                    Waves[a].EnemyList.Add(temp);
-                                    temp.GetComponent<Enemy_SpaceShip>().MoveAllowed = true;
-                                    temp.GetComponent<Enemy_SpaceShip>().FinalDestination =
-                                        Waves[a].FinalPositions[RandomFinalPositions].GetChild(temp1++);
+                                    temp.GetComponent<Enemy_SpaceShip>().Routes = new Transform[1];
+                                    temp.GetComponent<Enemy_SpaceShip>().Routes[0] = Waves[a].Routes[j];
                                 }
                             }
                         }
                     }
                 }
-                RandomRout = Random.Range(0, Rout.Length);
-                for (i = 0; i < Waves[a].EnemyList.Count; i++)
+            }
+
+            for (int j = 0; j < Waves[a].EnemyList.Count; j++)
+            {
+                if (Waves[a].EnemyList[j].activeInHierarchy)
                 {
-                    var Local = Waves[a].EnemyList[i];
-                        var localSecend = Local.GetComponent<Enemy_SpaceShip>();
-                        localSecend.coroutineAllowed = true;
-                        localSecend.MoveAllowed = true;
-                        //localSecend.tparam = 0;
-                        Local.GetComponent<Animator>().ResetTrigger("GotHit");
-                        Local.transform.position = transform.position;
-                        // Local.transform.rotation = transform.rotation;
-                        localSecend.MoveAllowed = true;
-                        localSecend.Routes = new Transform[1];
-                        localSecend.Routes[0] = Rout[RandomRout];
-                        Waves[a].EnemyList[i].SetActive(true);
-                        Debug.Log("InToshNis:)");
-                        dd = false;
-                        this.a = i;
-                        if (i==Waves[a].EnemyList.Count-1)
-                        {
-                            SpawnAllowed = false;
-                        }
-                        yield return new WaitForSeconds(Waves[a].ActiveDly);
+                    activEnemy.Add(Waves[a].EnemyList[j]);
+                }
+            }
+            RandomRout = Random.Range(0, Rout.Length);
+            for (i = 0; i < Waves[a].EnemyList.Count; i++)
+            {
+                WaveNumber = a;
+                var Local = Waves[a].EnemyList[i];
+                var localSecend = Local.GetComponent<Enemy_SpaceShip>();
+                localSecend.coroutineAllowed = true;
+                localSecend.MoveAllowed = true;
+                //localSecend.tparam = 0;
+                Local.GetComponent<Animator>().ResetTrigger("GotHit");
+                Local.transform.position = transform.position;
+                // Local.transform.rotation = transform.rotation;
+                localSecend.MoveAllowed = true;
+                // if (Waves[a].spetiallRout)
+                // {
+                //    // for (int j = 0; j < Waves[a].EnemyTypes.Length; ++j)
+                //    // {
+                //         localSecend.Routes = new Transform[1];
+                //         localSecend.Routes[0] = Waves[a].Routes[i];
+                //        // Waves[a].EnemyList[j].SetActive(true);
+                //    // }
+                // }
+               // for (int j = 0; j < Waves[a].EnemyTypes.Length; ++j)
+               // {
+               //     for (int k = 0; k < Waves[a].Quantity[j]; k++)
+               //     {
+               //         if (Waves[a].spetiallRout)
+               //         {
+               //             localSecend.Routes = new Transform[1];
+               //             localSecend.Routes[0] = Waves[a].Routes[j];
+               //         }
+               //     }
+               // }
+                RandomRout = Random.Range(0, Rout.Length);
+                if (!Waves[a].spetiallRout)
+                {
+                    localSecend.Routes = new Transform[1];
+                    localSecend.Routes[0] = Rout[RandomRout];
+                }
+               // localSecend.Routes = new Transform[1];
+               // localSecend.Routes[0] = Rout[RandomRout];
+                Waves[a].EnemyList[i].SetActive(true);
+                Debug.Log("InToshNis:)");
+                dd = false;
+                if (i == Waves[a].EnemyList.Count - 1)
+                {
+                    SpawnAllowed = false;
                 }
 
-                Debug.Log("InToshNisoooooooooo:)");
-
-                yield return null;
-
+               
+                yield return new WaitForSeconds(Waves[a].ActiveDly);
             }
+
+            Debug.Log("InToshNisoooooooooo:)");
+            yield return null;
         }
+    }
 
     bool CheckAlive()
     {
@@ -147,19 +209,17 @@ public class Random_Director : MonoBehaviour
 
     void GetRandomWaveIndex()
     {
-        if (!Pause)
-        {
-            FirstRandom = WaveNumber - 1;
+          FirstRandom = WaveNumber - 1;
             LastRandom = WaveNumber + 2;
             if (FirstRandom < 0) FirstRandom = 0;
             if (FirstRandom >= Waves.Length) FirstRandom = Waves.Length - 3;
             if (LastRandom > Waves.Length) LastRandom = Waves.Length;
+            RandomWave = Random.Range(0, 2);
             UpgrateWave = false;
             if (b != null) StopCoroutine(b);
             b = SpawnEnemyWaves(RandomWave);
             StartCoroutine(b);
-            RandomWave = Random.Range(FirstRandom, LastRandom);
-        }
     }
+
     #endregion
 }
---
+++ Arcade-Shooter/Assets/Scripts/Core_Scripts/ShopControler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopControler : MonoBehaviour
+{
+    private int MonyAmont;
+    private int IsItemSolid;
+
+    public Text MonyAmonttxt;
+    public Text ItemPrisetxt;
+
+    public Button BuyItemBTN;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MonyAmont = PlayerPrefs.GetInt("MonyAmount");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MonyAmonttxt.text = "Mony" + MonyAmont.ToString() + "$";
+        IsItemSolid = PlayerPrefs.GetInt("IsItemSolid");
+        if (MonyAmont>5&&IsItemSolid==0)
+        {
+            BuyItemBTN.interactable = true;
+        }
+        else
+        {
+            BuyItemBTN.interactable = false;
+        }
+    }
+
+    public void BuyItem()
+    {
+        MonyAmont = 5;
+        PlayerPrefs.SetInt("IsItemSolid",1);
+        ItemPrisetxt.text = "Solid";
+        BuyItemBTN.gameObject.SetActive(false);
+    }
+
+    public void ExitShop()
+    {
+        PlayerPrefs.SetInt("MonyAmount",MonyAmont);
+    }
+
+    public void ReastShop()
+    {
+        MonyAmont = 0;
+        BuyItemBTN.gameObject.SetActive(true);
+        ItemPrisetxt.text = "Prise:5";
+        PlayerPrefs.DeleteAll();
+    }
+}
